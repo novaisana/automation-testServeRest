4ff8b260b44acd1978cf072f60a694d0453b2008
Ana Carolina Novais <anacarolinanovais1@outlook.com>
2025-01-20 21:14:32 -0300

    remove env

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,9 @@
 const { defineConfig } = require('cypress');
-require('dotenv').config();
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    },
-    env: {
-      username: process.env.CYPRESS_USERNAME,
-      password: process.env.CYPRESS_PASSWORD
     }
   },
   
